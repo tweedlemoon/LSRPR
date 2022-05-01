ff7eea5bc00b4a2e55a0e1dd9e0c877b9b04afd2
小飞 <9130294+tweedlemoon@user.noreply.gitee.com>
2022-05-01 18:04:27 +0800

    visualization update

--- inference.py
+++ inference.py
@@ -13,6 +13,8 @@ import PIL
 from PIL import Image
 from torchvision import transforms
 
+from steps.make_data import MakeData
+
 img_path = Data_Path + "DRIVE/test/images/02_test.tif"
 roi_mask_path = Data_Path + "DRIVE/test/mask/02_test_mask.gif"
 ground_truth_path = Data_Path + "DRIVE/test/1st_manual/02_manual1.gif"
@@ -20,12 +22,16 @@ ground_truth_path = Data_Path + "DRIVE/test/1st_manual/02_manual1.gif"
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="inference your model.")
+    # batchsize是做数据的时候判断使用多少CPU核心时用的，其实在做验证集时并不需要
+    parser.add_argument("-b", "--batch-size", default=Batch_Size, type=int)
     parser.add_argument("--which-gpu", default=Which_GPU, type=str, help="which gpu to use")
     parser.add_argument("--data-path", default=Data_Root, help="data root")
     parser.add_argument("--device", default=str(Device), type=str, help="training device")
     # parser.add_argument('--model_path', default="save_weights/best_model_mine.pth", help="the best trained model root")
     parser.add_argument('--model_path', default="save_weights/best_model.pth", help="the best trained model root")
     parser.add_argument("--num-classes", default=Class_Num, type=int)
+    parser.add_argument("--dataset", default=Data_Name, type=str, choices=["voc2012", "DRIVE"],
+                        help="which dataset to use")
 
     return parser.parse_args()
 
@@ -40,40 +46,74 @@ def run_inference(args):
     pth = torch.load(args.model_path, map_location=device)
     model.load_state_dict(pth['model'])
 
-    # load picture
-    img_input = Image.open(img_path).convert('RGB')
-    val_range(name="input image", input_val=img_input)
-    data_transform = transforms.Compose([transforms.ToTensor(),
-                                         transforms.Normalize(mean=mean, std=std)])
-    img = data_transform(img_input)
-    # batch_size=1扩充出来
-    img = torch.unsqueeze(img, dim=0).to(device)
-    val_range("standard img", img)
+    # 单张图片测试
+    # # load picture
+    # img_input = Image.open(img_path).convert('RGB')
+    # val_range(name="input image", input_val=img_input)
+    # data_transform = transforms.Compose([transforms.ToTensor(),
+    #                                      transforms.Normalize(mean=mean, std=std)])
+    # img = data_transform(img_input)
+    # # batch_size=1扩充出来
+    # img = torch.unsqueeze(img, dim=0).to(device)
+    # val_range("standard img", img)
 
+    # 多张图片测试，直接制作dataloader
+    val_loader = MakeData(args=args).val_loader
     model.eval()
     with torch.no_grad():
-        # 送入网络后输出，把0维度弄掉，因为batch_size是1
-        net_output = model(img)['out']
-        val_range("Network output", net_output)
+        for idx, (img, real_result) in enumerate(val_loader, start=0):
+            original_img = val_loader.dataset.img_list[idx]
+            ground_truth = val_loader.dataset.manual[idx]
+            roi_mask = val_loader.dataset.roi_mask[idx]
+            original_img = Image.open(original_img)
+            ground_truth = Image.open(ground_truth)
+            # double_img_show(format_convert(original_img), format_convert(ground_truth))
 
-        # 进行argmax操作
-        argmax_output = net_output.argmax(1)
-        val_range("Argmax output", argmax_output)
-        # 注意此处必须先把tensor从gpu中拿到cpu才能转numpy
-        np_argmax_output = np.array(argmax_output.cpu())
-        # 0是黑，255是白，故要乘以255，0依旧是0,1则变成255
-        np_argmax_output = np_argmax_output.astype(np.uint8).squeeze(0) * 255
-        val_range("Numpy argmax output", np_argmax_output)
+            img = img.to(device)
+            net_output = model(img)['out']
+            val_range("Network output", net_output)
+            # 进行argmax操作
+            argmax_output = net_output.argmax(1)
+            val_range("Argmax output", argmax_output)
+            # 注意此处必须先把tensor从gpu中拿到cpu才能转numpy
+            np_argmax_output = np.array(argmax_output.cpu())
+            # 0是黑，255是白，故要乘以255，0依旧是0,1则变成255
+            np_argmax_output = np_argmax_output.astype(np.uint8).squeeze(0) * 255
+            val_range("Numpy argmax output", np_argmax_output)
 
-        # 把周围跟mask处理一下
-        # roi_img = Image.open(roi_mask_path).convert('L')
-        # roi_img = np.array(roi_img)
-        # np_argmax_output[roi_img == 0] = 0
+            # 把周围跟mask处理一下
+            roi_img = Image.open(roi_mask).convert('L')
+            roi_img = np.array(roi_img)
+            np_argmax_output[roi_img == 0] = 0
 
-        # 载入原图和标准gt，三者展示对比
-        original_img = Image.open(img_path)
-        ground_truth = Image.open(ground_truth_path)
-        triple_img_show(original_img, ground_truth, np_argmax_output)
+            triple_img_show(original_img=format_convert(original_img),
+                            original_mask=format_convert(ground_truth),
+                            predicted_img=format_convert(np_argmax_output))
+
+    # model.eval()
+    # with torch.no_grad():
+    #     # 送入网络后输出，把0维度弄掉，因为batch_size是1
+    #     net_output = model(img)['out']
+    #     val_range("Network output", net_output)
+    #
+    #     # 进行argmax操作
+    #     argmax_output = net_output.argmax(1)
+    #     val_range("Argmax output", argmax_output)
+    #     # 注意此处必须先把tensor从gpu中拿到cpu才能转numpy
+    #     np_argmax_output = np.array(argmax_output.cpu())
+    #     # 0是黑，255是白，故要乘以255，0依旧是0,1则变成255
+    #     np_argmax_output = np_argmax_output.astype(np.uint8).squeeze(0) * 255
+    #     val_range("Numpy argmax output", np_argmax_output)
+    #
+    #     # 把周围跟mask处理一下
+    #     roi_img = Image.open(roi_mask_path).convert('L')
+    #     roi_img = np.array(roi_img)
+    #     np_argmax_output[roi_img == 0] = 0
+    #
+    #     # 载入原图和标准gt，三者展示对比
+    #     original_img = Image.open(img_path)
+    #     ground_truth = Image.open(ground_truth_path)
+    #     triple_img_show(format_convert(original_img), format_convert(ground_truth), format_convert(np_argmax_output))
 
 
 if __name__ == '__main__':
--- utils/handy_functions.py
+++ utils/handy_functions.py
@@ -1,6 +1,8 @@
-import torch
-import numpy as np
+import PIL.Image
 import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
 from torchvision import transforms
 
 
@@ -35,36 +37,116 @@ def val_range(name: str, input_val):
     return min_val, max_val
 
 
-def img_show(img):
+def cv2_img_show(img):
     """
-    展示图片
+    使用opencv展示图片（不推荐）
     :param img:图片
     """
-    if img.type() == "torch.LongTensor":
-        img_numpy = np.array(img, dtype=np.uint8)
-    elif img.type() == "torch.FloatTensor":
-        img_numpy = np.array(img)
-    else:
-        return -1
-    # cv2.cvtColor(img_numpy, cv2.COLOR_BGR2RGB)
-    cv2.imshow("show img", img_numpy)
+    img = np.array(img)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    cv2.imshow("img", img)
     cv2.waitKey(0)
 
 
-def triple_img_show(original_img, original_mask, pridiction_img):
+def img_show(img):
     """
+    使用matplotlib展示图片
+    :param img:图片
+    """
+    plt.axis('off')
+    plt.imshow(img, cmap='gray')
+    plt.show()
+
+
+def double_img_show(input_img, predicted_img):
+    """
+    用于展示原图+预测结果
+    :param input_img: 输入图片
+    :param predicted_img: 预测结果
+    """
+    plt.subplot(121)
+    # 不显示坐标轴
+    plt.axis('off')
+    plt.imshow(input_img, cmap='gray')
+    plt.title('input image')
+
+    plt.subplot(122)
+    plt.axis('off')
+    plt.imshow(predicted_img, cmap='gray')
+    plt.title('prediction')
+
+    plt.show()
+
+
+def triple_img_show(original_img, original_mask, predicted_img):
+    """
+    用于展示原图，标准答案，预测结果
     :param original_img: 原图
     :param original_mask: 原mask
-    :param pridiction_img: 预测结果
+    :param predicted_img: 预测结果
     """
-    original_img = np.array(original_img)
-    original_mask = np.array(original_mask)
-    pridiction_img = np.array(pridiction_img)
-    original_img = cv2.cvtColor(original_img, cv2.COLOR_BGR2RGB)
-    original_mask = cv2.cvtColor(original_mask, cv2.COLOR_BGR2RGB)
-    pridiction_img = cv2.cvtColor(pridiction_img, cv2.COLOR_BGR2RGB)
-    # 展示图片
-    cv2.imshow("Original image", original_img)
-    cv2.imshow("Original mask", original_mask)
-    cv2.imshow("Prediction", pridiction_img)
-    cv2.waitKey(0)
+    plt.subplot(131)
+    # 不显示坐标轴
+    plt.axis('off')
+    plt.imshow(original_img, cmap='gray')
+    plt.title('original image')
+
+    plt.subplot(132)
+    plt.axis('off')
+    plt.imshow(original_mask, cmap='gray')
+    plt.title('original mask')
+
+    plt.subplot(133)
+    plt.axis('off')
+    plt.imshow(predicted_img, cmap='gray')
+    plt.title('prediction')
+
+    plt.show()
+
+
+def format_convert(input_format):
+    """
+    :param input: 可能是tensor，可能是PIL，可能是Numpy
+    :return: 一张PIL图片
+    """
+    pil_output = None
+    if type(input_format) == torch.Tensor:
+        if (input_format.shape.__len__() == 4 and input_format.shape[0] == 1) \
+                or (input_format.shape.__len__() == 3 and input_format.shape[0] == 1):
+            # 证明这个带了batchsize
+            input_format = input_format.squeeze(0)
+            input_format = input_format.type(torch.uint8)
+            pil_output = transforms.ToPILImage()(input_format)
+            pil_output = pil_output.convert('RGB')
+        elif ((input_format.shape[0] == 3 or input_format.shape[0] == 4) and input_format.shape.__len__() == 3) \
+                or input_format.shape.__len__() == 2:
+            pil_output = transforms.ToPILImage()(input_format)
+            pil_output = pil_output.convert('RGB')
+        else:
+            raise TypeError("The input is not a picture.")
+    elif type(input_format) == np.ndarray:
+        pil_output = PIL.Image.fromarray(np.uint8(input_format))
+        pil_output = pil_output.convert('RGB')
+    else:
+        pil_output = input_format
+        pil_output = pil_output.convert('RGB')
+    return pil_output
+
+
+if __name__ == '__main__':
+    pic = '../cache/testimg/deeplab1.png'
+    img_PIL = PIL.Image.open(pic)
+    img_show(format_convert(img_PIL))
+    # img_show(img_PIL)
+
+    img_PIL = img_PIL.convert('RGB')
+
+    img_opencv = cv2.imread(pic)
+    img_opencv = cv2.cvtColor(img_opencv, cv2.COLOR_BGR2RGB)
+
+    img_plt = plt.imread(pic)
+
+    img_tensor = transforms.ToTensor()(img_PIL)
+    img_numpy = np.array(img_PIL)
+    val_range('tensor img', img_numpy)
+    pass
