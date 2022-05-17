f97922328b9d24edf68c00834ceaf8d71e40a77d
小飞 <9130294+tweedlemoon@user.noreply.gitee.com>
2022-05-17 16:37:19 +0800

    udpate poweroff

--- run.sh
+++ run.sh
@@ -3,29 +3,29 @@ cd /liuyifei/LSFforSeg
 python trainLSF.py \
   --data-path "/root/autodl-tmp/liuyifei" \
   -b 2 \
-  --epochs 200 \
+  --epochs 300 \
   --loss-weight 1.0 2.0 \
   --back-bone unet
 
 python trainLSF.py \
   --data-path "/root/autodl-tmp/liuyifei" \
   -b 2 \
-  --epochs 200 \
-  --level-set-coe 0.000005 \
+  --epochs 300 \
+  --level-set-coe 0.000001 \
   --loss-weight 1.0 2.0 \
   --back-bone unet
 
 python trainLSF.py \
   --data-path "/root/autodl-tmp/liuyifei" \
   -b 2 \
-  --epochs 200 \
+  --epochs 300 \
   --loss-weight 1.0 2.0 \
   --back-bone r2unet
 
 python trainLSF.py \
   --data-path "/root/autodl-tmp/liuyifei" \
   -b 2 \
-  --epochs 200 \
+  --epochs 300 \
   --level-set-coe 0.000001 \
   --loss-weight 1.0 2.0 \
   --back-bone r2unet
@@ -33,14 +33,14 @@ python trainLSF.py \
 python trainLSF.py \
   --data-path "/root/autodl-tmp/liuyifei" \
   -b 2 \
-  --epochs 200 \
+  --epochs 300 \
   --loss-weight 1.0 2.0 \
   --back-bone attunet
 
 python trainLSF.py \
   --data-path "/root/autodl-tmp/liuyifei" \
   -b 2 \
-  --epochs 200 \
+  --epochs 300 \
   --level-set-coe 0.000001 \
   --loss-weight 1.0 2.0 \
   --back-bone attunet
@@ -48,16 +48,18 @@ python trainLSF.py \
 python trainLSF.py \
   --data-path "/root/autodl-tmp/liuyifei" \
   -b 2 \
-  --epochs 200 \
+  --epochs 300 \
   --loss-weight 1.0 2.0 \
   --back-bone r2attunet
 
 python trainLSF.py \
   --data-path "/root/autodl-tmp/liuyifei" \
   -b 2 \
-  --epochs 200 \
+  --epochs 300 \
   --level-set-coe 0.000001 \
   --loss-weight 1.0 2.0 \
   --back-bone r2attunet
 
 python utils/emailSender.py
+
+sudo shutdown -h now
