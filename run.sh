b02eb862a14e88f9e29faa42827a5819c9abe5cf
小飞 <9130294+tweedlemoon@user.noreply.gitee.com>
2022-06-07 11:16:14 +0800

    alter run

--- run.sh
+++ run.sh
@@ -1,41 +1,13 @@
-cd /liuyifei/LSFforSeg
-
 python trainLSF.py \
-  --data-path "/root/autodl-tmp/liuyifei" \
+  --which-gpu 0 \
+  --device cuda \
   --dataset DRIVE \
-  -b 2 \
-  --epochs 200 \
+  --back-bone attunetplus \
+  --data-path "/root/autodl-tmp/liuyifei" \
   --level-set-coe 0 \
   --loss-weight 1.0 2.0 \
-  --back-bone attunetplus
-
-python trainLSF.py \
-  --data-path "/root/autodl-tmp/liuyifei" \
-  --dataset DRIVE \
-  -b 2 \
+  --batch-size 2 \
   --epochs 300 \
-  --level-set-coe 0.000001 \
-  --loss-weight 1.0 2.0 \
-  --back-bone attunetplus
-
-python trainLSF.py \
-  --data-path "/root/autodl-tmp/liuyifei" \
-  --dataset Chase_db1 \
-  -b 2 \
-  --epochs 200 \
-  --level-set-coe 0 \
-  --loss-weight 1.0 2.0 \
-  --back-bone attunetplus
-
-python trainLSF.py \
-  --data-path "/root/autodl-tmp/liuyifei" \
-  --dataset Chase_db1 \
-  -b 2 \
-  --epochs 300 \
-  --level-set-coe 0.000001 \
-  --loss-weight 1.0 2.0 \
-  --back-bone attunetplus
-
-python utils/emailSender.py
+  --lr 0.01
 
 shutdown -h now
